840eaf53f8e74f839f3cf5dc484414e83f30a204
kylin <qlgxljz@sina.com>
2021-01-20 01:54:49 +0800

    task08-2

--- task08-2/src/Solution.java
+++ task08-2/src/Solution.java
@@ -5,4 +5,9 @@
  * @date 2021/1/20
  */
 public class Solution {
+    public int climbStairs(int n) {
+        double sqrt_5 = Math.sqrt(5);
+        double fib_n = Math.pow((1 + sqrt_5) / 2, n + 1) - Math.pow((1 - sqrt_5) / 2, n + 1);
+        return (int) (fib_n / sqrt_5);
+    }
 }
